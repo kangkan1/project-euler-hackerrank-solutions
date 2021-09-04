d6f237c5d83a97fb848f31e1be8a141b5e7ff95e
Kangkan Lahkar <kangkanlahkar1@gmail.com>
2021-09-04 23:13:28 +0530

    solution 16 python, powers of 2 in O(1) complexity

---
+++ Project Euler #16/Python/solution16.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        num = 1 << n
+        s = 0
+        while num > 0:
+            s = s+num%10
+            num = num//10
+        print(s)
